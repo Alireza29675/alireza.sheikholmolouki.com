63e6b8322f3c366059f14f231c64f166a94ae397
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-09-14 02:04:04 +0430

    First client-side scripting configuration has been done

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+class App {
+    constructor() {
+        console.log(THREE)
+    }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello');
+import App from './components/App'
+
+window.$ = (query) => document.querySelector(query);
+window.$$ = (query) => document.querySelectorAll(query);
+
+window.after = (time, doWhat) => setTimeout(doWhat, time);
+window.every = (time, doWhat) => setInterval(doWhat, time);
+
+window.app = new App;
