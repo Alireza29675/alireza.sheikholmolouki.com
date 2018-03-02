e2551141c74960ca91fc10a3b27db76167ddf5c6
Alireza29675 <alireza.sheikholmolouki@gmail.com>
2018-03-02 14:56:19 +0330

    some package.json and gitignore updates

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'Alireza Sheikholmolouki | Real-Time Experiences Developer'
+    title: 'Alireza Sheikholmolouki | Art Lover, Binaries Fan'
   });
 });
 
