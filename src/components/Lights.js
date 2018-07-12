913e9fcb981a77cb359b32c42bc965f38171af8a
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2018-07-12 16:43:14 +0430

    some styles improvements

--- src/components/Lights.js
+++ src/components/Lights.js
@@ -1,10 +1,11 @@
 // define lights
 const lights = {
     globalAmbient: new THREE.AmbientLight(0xffffff, 0.5),
-    topLight: new THREE.PointLight(0xffffff, 0.6)
+    topLight: new THREE.PointLight(0xffffff, 0.9)
 }
 // Changing positions and etc
 lights.topLight.position.y = 100
-lights.topLight.position.z = -1000
+lights.topLight.position.z = -700
+
 // exporting all of them
 export default lights
--- src/components/Surface.js
+++ src/components/Surface.js
@@ -1,7 +1,20 @@
 class Surface {
     constructor () {
+        this.speed = 1;
+        this.power = 1;
+        this.y = 0;
+        window.addEventListener('mousedown', () => {
+            this.speed = 0.8;
+            this.power = 10;
+            this.y = -2;
+        });
+        window.addEventListener('mouseup', () => {
+            this.speed = 1;
+            this.power = 1;
+            this.y = 0;
+        });
         this.frame = 0
-        this.geometry = new THREE.PlaneGeometry(1000, 1000, 80, 80)
+        this.geometry = new THREE.PlaneGeometry(800, 800, 80, 80)
         this.material = new THREE.MeshPhongMaterial({ color: 0x333333, wireframe: true })
         this.mesh = new THREE.Mesh(this.geometry, this.material)
         this.mesh.rotation.x = -Math.PI / 2
@@ -13,12 +26,21 @@ class Surface {
     }
     setVertices () {
         const w = window.innerWidth, h = window.innerHeight, x = window.MOUSE.x, y = window.MOUSE.y
-        for (let [i, vertice] of this.geometry.vertices.entries()) {
-            vertice.z = this.initalVertices[i].z + Math.sin((this.frame / 20) + i)
+        const allVertices = this.geometry.vertices.entries();
+        for (let [i, vertice] of allVertices) {
+            vertice.z = this.initalVertices[i].z + Math.sin((this.frame / 20 * this._speed) + i) * this._power;
         }
     }
+    smoothVars () {
+        if (!this._speed) this._speed = this.speed;
+        if (!this._power) this._power = this.power;
+        this._power += (this.power - this._power) / 10;
+        this._speed += (this.speed - this._speed) / 10;
+        this.mesh.position.y += (this.y - this.mesh.position.y) / 10;
+    }
     render () {
-        this.frame++
+        this.frame++;
+        this.smoothVars();
         this.setVertices()
         this.geometry.verticesNeedUpdate = true;
     }
