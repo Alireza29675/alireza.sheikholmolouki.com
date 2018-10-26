207e485dd84d4ff540271881270d827cd0cf06ed
Alireza29675 <alireza.sheikholmolouki.com>
2018-10-26 04:22:30 +0330

    Tray was made

--- src/components/Taskbar/Taskbar.js
+++ src/components/Taskbar/Taskbar.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
 import './Taskbar.css';
 import Button from '../../sub-components/Button/Button';
-import { relative } from 'path';
+import Frame from '../../sub-components/Frame/Frame';
 
 class Taskbar extends Component {
+
+    constructor (props) {
+        super(props);
+        this.state = {
+            time: null,
+            soundOn: true
+        }
+    }
+
+    componentDidMount () {
+        this.setTime();
+        this.clockInterval = setInterval(this.setTime.bind(this), 10 * 1000);
+    }
+
+    componentWillUnmount () {
+        clearInterval(this.clockInterval);
+    }
+
+    setTime () {
+        const time = (new Date()).toLocaleString('en-US', {
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true
+        });
+        this.setState({ time })
+    }
+
+    toggleSound () {
+        this.setState({ soundOn: !this.state.soundOn })
+    }
+
     render() {
         return (
             <div className="taskbar">
@@ -11,6 +42,15 @@ class Taskbar extends Component {
                     <img src="./images/start.png" alt="Start Icon" />
                     <b>Start</b>
                 </Button>
+                <div className="tasks">
+                    
+                </div>
+                <Frame className="tray">
+                    <img className="sound icon"
+                        onClick={this.toggleSound.bind(this)}
+                        src={`./images/icons/audio-${this.state.soundOn ? 'on' : 'off'}.png`} />
+                    <time>{ this.state.time }</time>
+                </Frame>
             </div>
         );
     }
