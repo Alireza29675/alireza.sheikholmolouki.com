546ae4136758abfca1466554074b450ef14940f8
Alireza29675 <alireza.sheikholmolouki.com>
2018-10-26 04:22:16 +0330

    Frame was completed

--- src/sub-components/Frame/Frame.js
+++ src/sub-components/Frame/Frame.js
@@ -4,6 +4,7 @@ import './Frame.css';
 class Frame extends Component {
 
     render () {
+        const { style, children, className } = this.props;
         return (
             <div
                 className={"frame-98" + (className ? ` ${className}` : '')}
